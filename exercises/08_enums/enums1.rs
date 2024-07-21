3b4eaa623ea1fe19d4d03ff2c73d78849ebd9b45
msyvr <15218145+msyvr@users.noreply.github.com>
2024-07-20 23:53:39 -0700

    enums

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,9 +1,15 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Move,
+    Echo,
+    ChangeColor,
+    Quit,
 }
 
 fn main() {
+
     println!("{:?}", Message::Resize);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::Echo);
